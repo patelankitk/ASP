3c328d91292347f95a9e38ed85b56a7226d5ee76
patelankitk <patelankitk@yahoo.com>
2016-05-09 22:15:24 +0100

    ADD

---
+++ Login/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ButtonSubmit_Click(object sender, EventArgs e)
+    {
+        LabelSubmit.Text = "Your Registration was successful.!";
+    }
+
+    protected void DropDownListCountry_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
