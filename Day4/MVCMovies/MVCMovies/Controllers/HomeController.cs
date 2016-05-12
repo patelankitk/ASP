938b22d5ebf4d89e60be6c31d3983c41b45fda64
patelankitk <patelankitk@yahoo.com>
2016-05-12 16:26:22 +0100

    Add

---
+++ Day4/MVCMovies/MVCMovies/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using MVCMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity;
+
+namespace MVCMovies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MvcMoviesDataBaseEntities db = new MvcMoviesDataBaseEntities();
+
+        public ActionResult Index()
+        {
+            var movies = from m in db.Movies select m;
+            return View(movies);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+
+            if (movie==null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include ="ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            db.Entry(movie).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int ? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public ActionResult DeleteConfirmed(int ? id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            db.Movies.Add(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
